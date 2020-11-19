bb632ea96bf1832b3e681c40e8b5a2e956a6a35c
Luis Felipe Cerero Garcia <luisfelipe.cerero@kycus.io>
2020-11-18 21:28:52 -0500

    Loaders, plugins

---
+++ css-style-loader/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            // Se cargan los loaders
+            {
+                // test: Tipo de archivo a reconocer.
+                // use: Que loader se va encargar del archivo.
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+}
