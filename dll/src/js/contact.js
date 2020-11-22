139da88514853514810981035053aa1dfa6e3347
Luis Felipe Cerero Garcia <luisfelipe.cerero@kycus.io>
2020-11-22 18:23:45 -0500

    Cargando modulos, para rendimiento a la hora de desarrollador con dll y dllreferences, ademas se deja una opcion con wepack version 5, que sirve para importar de otra manera los vendors o los node-modules.

---
+++ dll/src/js/contact.js
@@ -0,0 +1,39 @@
+import '../css/estilos.css'
+import { firstMessage, delayMessage } from './message.js'
+// import platziImg from '../img/platzi.png';
+// import platziVideo from '../video/que-es-core.mp4'
+import exampleJson from '../json/example.json';
+// import renderDom from '../js/render.js';
+// import '../css/main.less'
+
+import Teachers from './components/teachers.js';
+
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<Teachers data={exampleJson} />, document.getElementById('container'));
+
+
+// // images
+// const img = document.createElement('img');
+// img.setAttribute('src', platziImg);
+// img.setAttribute('height', 50);
+// img.setAttribute('width', 50);
+// renderDom(img);
+
+// // video
+// const video = document.createElement('video');
+// video.setAttribute('src', platziVideo);
+// video.setAttribute('width', 480);
+// video.setAttribute('controls', true);
+// video.setAttribute('autoplay', true);
+// renderDom(video);
+
+// exampleJson.teachers.forEach((teacher) => {
+//     const element = document.createElement("li");
+//     element.textContent = teacher.name;
+//     renderDom(element);
+// });
+
+document.write(firstMessage);
+delayMessage();
