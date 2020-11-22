a4d942889becedee661b59e052d38c172b08b591
Luis Felipe Cerero Garcia <luisfelipe.cerero@kycus.io>
2020-11-22 13:06:57 -0500

    soporte a react

---
+++ react/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/js/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["es2015", "react"]
+                    }
+                }
+            },
+            // Se cargan los loaders
+            {
+                // test: Tipo de archivo a reconocer.
+                // use: Que loader se va encargar del archivo.
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|woff|eot|ttf|svg|otf)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 255000//limite de peso del archivo. BITS 
+                    }
+                }
+            },
+            {
+                test: /\.(mp4|webm)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1000000,
+                        name: 'video/[name].[hash].[ext]'
+                    }
+                }
+            },
+            {
+                test: /\.json$/
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css'
+        })
+    ]
+}
