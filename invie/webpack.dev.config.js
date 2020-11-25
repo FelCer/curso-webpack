bed76dfa424872a2a96dcd17ac03f02a39720797
Luis Felipe Cerero Garcia <luisfelipe.cerero@kycus.io>
2020-11-24 20:57:02 -0500

    projecto de ejemplo en entorno development

---
+++ invie/webpack.dev.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        home: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js|jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpg|gif|woff|eot|ttf|svg|otf)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 255000,//limite de peso del archivo. BITS 
+                        fallback: 'file-loader',
+                        name: 'images/[name].[hash].[ext]'
+                    }
+                }
+            },
+        ]
+    },
+    devServer: {
+        //contentBase: './dist',
+        port: 8080,
+        publicPath: '/'
+    },
+}
